bde78bf3d48745db8201d37cfa08e0d010bb21f6
zhangrq <zhangrq@fenbi.com>
2019-07-16 01:42:17 +0800

    delete

--- src/app/utils/event.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from 'events';
-
-/**
- * 用内置EventEmitter
- */
-export class StateEvent extends EventEmitter {}
-export const stateEvent = new StateEvent();
--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,1 @@
-export * from './decorator';
-export * from './state';
-export * from './sugar';
+// 1
